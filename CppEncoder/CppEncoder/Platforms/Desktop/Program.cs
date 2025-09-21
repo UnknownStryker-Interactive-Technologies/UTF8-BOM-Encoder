de9d3954167a19a3012d74304b91690ab44627a9
Unknown-Stryker <leehojin6580@gmail.com>
2025-09-22 03:20:00 +0900

    Add the AGPL3 copyright notice
    Copyright © from 2022 to present, UNKNOWN STRYKER. All Rights Reserved.
    Licensed under the AGPLv3 License. You may not use this file except in compliance with the License.

--- CppEncoder/CppEncoder/Platforms/Desktop/Program.cs
+++ CppEncoder/CppEncoder/Platforms/Desktop/Program.cs
@@ -1,4 +1,11 @@
 using Uno.UI.Hosting;
+/*
+Copyright © from 2022 to present, UNKNOWN STRYKER. All Rights Reserved.
+Licensed under the AGPLv3 License. You may not use this file except in compliance with the License.
+*/
+
+
+
 
 namespace CppEncoder;
 internal class Program
--- CppEncoder/CppEncoder/Presentation/MainPage.xaml.cs
+++ CppEncoder/CppEncoder/Presentation/MainPage.xaml.cs
@@ -4,6 +4,10 @@ using System.Text;
 using System.Threading;
 using Ude;
 using Windows.Storage.Pickers;
+/*
+Copyright © from 2022 to present, UNKNOWN STRYKER. All Rights Reserved.
+Licensed under the AGPLv3 License. You may not use this file except in compliance with the License.
+*/
 
 
 
